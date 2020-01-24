3298c7d5f1daf8c6bd9b559ff1cf387153c98611
Raka Dean <rakache@gmail.com>
2020-01-24 12:09:45 +0700

    add SP

--- ToDoList/app/src/main/java/com/aditas/todolist/MainActivity.java
+++ ToDoList/app/src/main/java/com/aditas/todolist/MainActivity.java
@@ -3,7 +3,9 @@ package com.aditas.todolist;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -16,16 +18,25 @@ import android.widget.ListView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+    private final static String PREFERENCES = "MAIN PREF";
+    private final static String TODO_LIST = "TODO";
+
     private ArrayList <String> data;
     private ArrayAdapter <String> dataAdapter;
     private ListView lvData;
+    private SharedPreferences pref;
+    private Set <String> setList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pref = getSharedPreferences(PREFERENCES, MODE_PRIVATE);
+
         lvData = findViewById(R.id.lv_list);
         data = new ArrayList<String>();
         dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
@@ -45,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
                             }
                         })
                         .setNegativeButton("Ora",null).show();
+                savePreference();;
                 return true;
             }
         });
@@ -63,6 +75,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addTask(){
+        @SuppressLint("InflateParams")
+        final View view = this.getLayoutInflater().inflate(R.layout.dialog_add, null);
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Add task");
         builder.setMessage("What do you want?");
@@ -72,9 +86,18 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 data.add(inputField.getText().toString());
+                savePreference();
             }
         });
         builder.setNegativeButton("Cancel", null);
         builder.create().show();
+    }
+
+    private void savePreference(){
+        setList = new HashSet<>(data);
+        @SuppressLint("CommitPrefEdits")
+        SharedPreferences.Editor editor = pref.edit();
+        editor.putStringSet(TODO_LIST, setList);
+        editor.apply();
     }
 }
